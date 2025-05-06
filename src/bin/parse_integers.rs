8a3040c4f633148adba4042d92c57817090073c9
olivia <olivia@email.com>
2025-05-06 17:44:34 -0400

    yep

--- src/bin/parse_integers.rs
+++ src/bin/parse_integers.rs
@@ -2,10 +2,26 @@ use std::io::{self, BufRead};
 use std::os::unix::io::FromRawFd;
 fn parse(bytes: &[u8]) -> u32 {
     let mut num = 0;
-    for i in bytes.iter() {
-        num = num * 10 + (*i - b'0') as u32;
+    // for ijj
+    //     num = num * 10 + (*i - b'0') as u32;
+    // }
+    let mut num1 = 0;
+    let mut num2 = 0;
+    let mut num3 = 0;
+    let num1_vals = [0,0,1_000_000_000,100_000_000];
+    let num2_vals = [100000000,1000000,100000,10000];
+    let num3_vals = [1000,100,10,1];
+    let mut j = 0; 
+    for i in (3..16).step_by(4) {
+        println!("{} {}", i, bytes[i]);
+        num1 = num1 * num1_vals[j] + (bytes[i] - b'0') as u32;
+        num2 = num2 * num2_vals[j] + (bytes[i] - b'0') as u32;
+        num3 = num3 * num3_vals[j] + (bytes[i] - b'0') as u32;
+
+        j+=1;
     }
-    return num;
+    println!("{} {} {}", num1, num2, num3);
+    return num1 + num2 + num3;
 }
 
 fn main() {
@@ -19,7 +35,8 @@ fn main() {
         let num = parse(&buffer[..last]);
         sum += num as u64;
         buffer.clear();
+        break;
     }
 
-    println!("{}", sum);
+    println!("sum: {}", sum);
 }
